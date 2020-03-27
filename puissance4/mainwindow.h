05df64e3ba885268625d7607b6b0b21477f4fc0c
ESCOBAR Jérémy <jeremy.escobar@ynov.com>
2020-03-27 13:03:04 +0100

    Bas du jeux sans regles

--- puissance4/mainwindow.h
+++ puissance4/mainwindow.h
@@ -2,20 +2,42 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDialog>
+#include <QList>
+#include <QWidget>
+#include <QObject>
+#include <QLabel>
 
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
 
-class MainWindow : public QMainWindow
+class MainWindow : public QDialog
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    bool partieACommence();
+    void disableAllButtonColumn();
 
+private slots:
+    void on_pushButton_1_clicked();
+    void on_pushButton_2_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_4_clicked();
+    void on_pushButton_5_clicked();
+    void on_pushButton_6_clicked();
+    void on_pushButton_7_clicked();
+
+    void on_pushButton_clicked();
 private:
     Ui::MainWindow *ui;
+    QLabel *listeLabel;
+    QList<QLabel*> listeLabelList;
+    bool auJoueurDeJouer;
+    bool ordiJaune;
+
 };
 #endif // MAINWINDOW_H
