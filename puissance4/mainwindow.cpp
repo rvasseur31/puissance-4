5151ced348fee8a7a5a507672ca7bbbfdb7c19e4
rafi.31 <raphael.vasseur@ynov.com>
2020-04-10 09:56:26 +0200

    Clean project

--- puissance4/mainwindow.cpp
+++ puissance4/mainwindow.cpp
@@ -1,69 +1,20 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-
 #include <QDebug>
 
-
-
 MainWindow::MainWindow(QWidget *parent) :
-    QDialog(parent), ui(new Ui::MainWindow), auJoueurDeJouer(false)
+    QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    setWindowTitle("Puissance 5");
-    setMinimumSize(468, 525);
-    setModal(true);
-
-    int k=0, j=0;
-
-    for(int i=1;i<7;i++){
-        for(j=0;j<7;j++){
-            listeLabelList.push_back(new QLabel());
-            ui->gridLayout->addWidget(listeLabelList[k],i,j);
-            listeLabelList[k]->setStyleSheet("background-color:#FFFFF; border-radius:34px;");
-            k++;
-        }
-    }
-    ordiJaune = false;
 }
 
 
-MainWindow::~MainWindow()
-{
-    for(int i=0;i<42;i++){
-        delete listeLabelList[i];
-        listeLabelList[i]=0;
-    }
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-bool MainWindow::partieACommence(){
-    if(listeLabelList[35]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[36]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[37]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[38]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[39]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[40]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-    else if(listeLabelList[41]->styleSheet()!="background-color: #FFFFF; border-radius:34px;")
-        return true;
-   return false;
-}
 
-void MainWindow::disableAllButtonColumn(){
-    ui->pushButton_1->setDisabled(true);
-    ui->pushButton_2->setDisabled(true);
-    ui->pushButton_3->setDisabled(true);
-    ui->pushButton_4->setDisabled(true);
-    ui->pushButton_5->setDisabled(true);
-    ui->pushButton_6->setDisabled(true);
-    ui->pushButton_7->setDisabled(true);
-    //ui->pushButton->setDisabled(false)
-}
+
+
 
--- puissance4/mainwindow.h
+++ puissance4/mainwindow.h
@@ -12,32 +12,16 @@ namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QDialog
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    bool partieACommence();
-    void disableAllButtonColumn();
 
-private slots:
-    void on_pushButton_1_clicked();
-    void on_pushButton_2_clicked();
-    void on_pushButton_3_clicked();
-    void on_pushButton_4_clicked();
-    void on_pushButton_5_clicked();
-    void on_pushButton_6_clicked();
-    void on_pushButton_7_clicked();
-
-    void on_pushButton_clicked();
 private:
     Ui::MainWindow *ui;
-    QLabel *listeLabel;
-    QList<QLabel*> listeLabelList;
-    bool auJoueurDeJouer;
-    bool ordiJaune;
 
 };
 #endif // MAINWINDOW_H
