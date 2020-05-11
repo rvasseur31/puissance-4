ec1c79a3f1773ca913a1568e28f5ccf03c945322
jérémy <jeremy.escobar@ynov.com>
2020-05-11 13:03:06 +0200

    initialisation de la matrice x42 + création des class participant et matrix

---
+++ puissance4/matrixgame.h
@@ -0,0 +1,21 @@
+#ifndef MATRIXGAME_H
+#define MATRIXGAME_H
+
+#include <QMainWindow>
+#include <QObject>
+#include <QWidget>
+#include<iostream>
+#include<vector>
+
+
+class MatrixGame
+{
+
+public:
+    MatrixGame();
+    gamePlateau();
+};
+
+#endif // MATRIXGAME_H
+
+
